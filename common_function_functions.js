dd5593e70de7d307919af8826296a20636e6fc8b
xuxingxing <973553289@qq.com>
2020-06-09 22:19:14 +0800

    add get_pic_natural_size

--- common_function_functions.js
+++ common_function_functions.js
@@ -12,3 +12,21 @@ function judgeRepeatItem (arr) {
   }
   return false;
 }
+
+/*
+  2.The wrapper function is used to obtain the true width and height of the picture
+  description: Function packages compatible with IE7+ browser:
+*/
+  function getNaturalSize (DomElement) {
+    var natureSize = {};
+    if(window.naturalWidth && window.naturalHeight) {
+        natureSize.width = DomElement.naturalWidth;
+        natureSizeheight = DomElement.naturalHeight;
+    } else {
+    var img = new Image();
+        img.src = DomElement.src;
+        natureSize.width = img.width;
+        natureSize.height = img.height;
+    }
+    return natureSize;
+  }
